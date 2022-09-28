cce3520aa1cd6e069b53af133c157f69f456358d
Carl Justin gamos <gamoscj123@gmail.com>
2022-09-28 22:26:37 +0800

    Added Auto Translate to Tagalog

--- api/main-tf-serving.py
+++ api/main-tf-serving.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import requests
+import translators as ts
 
 app = FastAPI()
 
@@ -37,6 +38,10 @@ async def ping():
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
+
+def translate(message):
+    translated_message = ts.google(message, from_language='en', to_language='tl')
+    return translated_message
 
 def getSolution(predicted):
     if "Early Blight" in predicted:
@@ -77,37 +82,37 @@ def getSolution(predicted):
 
 def getDefinition(predicted):
     if "Early Blight" in predicted:
-        return "Early Blight: refers to any of a variety of plant blights whose symptoms start to appear earlier in the growing season. a: a fungal disease leaf spot, especially on potatoes and tomatoes (Alternaria solani)"
+        return "refers to any of a variety of plant blights whose symptoms start to appear earlier in the growing season. a: a fungal disease leaf spot, especially on potatoes and tomatoes (Alternaria solani)"
 
     elif "Healthy" in predicted:
         return "Healthy Crop!"
 
     elif "Late Blight" in predicted:
-        return "Late Blight: The solanaceous plant disease known as Late Blight, which affects plants such as the potato and tomato, is caused on by the fungus Phytophthora infestans. It is characterized by the weakening of stems, leaves, and, in the case of potatoes, tubers."
+        return "The solanaceous plant disease known as Late Blight, which affects plants such as the potato and tomato, is caused on by the fungus Phytophthora infestans. It is characterized by the weakening of stems, leaves, and, in the case of potatoes, tubers."
 
     elif "Blight" in predicted:
-        return "Blight: any of a variety of plant diseases that cause leaves, flowers, fruit, stems, or the entire plant to suddenly and severely yellow, brown, spot, wither, or die. The majority of blights are brought on by bacterial or fungal infestations, which typically target young, growing fast parts of the plant including shoots."
+        return "any of a variety of plant diseases that cause leaves, flowers, fruit, stems, or the entire plant to suddenly and severely yellow, brown, spot, wither, or die. The majority of blights are brought on by bacterial or fungal infestations, which typically target young, growing fast parts of the plant including shoots."
 
     elif "Common Rust" in predicted:
-        return "Common Rust: Every growing season, the fungus Puccinia sorghi causes Common rust. In hybrid corn, it rarely causes problems. Late June is usually when rust pustules start appear. Chlorotic spots on the surface of the leaf are one of the first signs of common rust. "
+        return "Every growing season, the fungus Puccinia sorghi causes Common rust. In hybrid corn, it rarely causes problems. Late June is usually when rust pustules start appear. Chlorotic spots on the surface of the leaf are one of the first signs of common rust. "
 
     elif "Gray Leaf Spot" in predicted:
-        return "Gray Leaf Spot: During every growing season, the fungus Cercospora zeae-maydis, which causes gray leaf spot, manifests itself. Economic losses could happen if the development of a disease is encouraged. About two to three weeks prior to tasseling, lower leaves first carry the risk of the condition."
+        return "During every growing season, the fungus Cercospora zeae-maydis, which causes gray leaf spot, manifests itself. Economic losses could happen if the development of a disease is encouraged. About two to three weeks prior to tasseling, lower leaves first carry the risk of the condition."
 
     elif "Bacterial Leaf Blight" in predicted:
-        return "Bacterial leaf blight: also known as bacterial blight of rice, is among the most destructive diseases to affect agricultural rice (Oryza sativa and O. glaberrima). A severe disease can result in crop losses of up to 75%, and millions of hectares of rice are afflicted every year"
+        return "also known as bacterial blight of rice, is among the most destructive diseases to affect agricultural rice (Oryza sativa and O. glaberrima). A severe disease can result in crop losses of up to 75%, and millions of hectares of rice are afflicted every year"
 
     elif "Brown Spot" in predicted:
-        return "Brown spot: A fungus called brown spot attacks the coleoptile, leaves, leaf sheath, panicle branches, glumes, and spikelets. The multiple different blotches on the leaves that really can kill the entire leaf are the most recognizable damage. Unfilled grains or spotted or discolored seeds develop when the seed becomes infected."
+        return "A fungus called brown spot attacks the coleoptile, leaves, leaf sheath, panicle branches, glumes, and spikelets. The multiple different blotches on the leaves that really can kill the entire leaf are the most recognizable damage. Unfilled grains or spotted or discolored seeds develop when the seed becomes infected."
 
     elif "Leaf Smut" in predicted:
-        return "Leaf smut: is a fungal infection caused by the fungus Entyloma oryzae. Rice disease that is widely distributed but relatively insignificant. The fungus generates slightly raised, angular, black spots. Both sides of the leaves have spots."
+        return "is a fungal infection caused by the fungus Entyloma oryzae. Rice disease that is widely distributed but relatively insignificant. The fungus generates slightly raised, angular, black spots. Both sides of the leaves have spots."
 
     elif "Septoria" in predicted:
-        return "Septoria blight causes numerous small brown spots (about 1/8' to 1/4' in diameter) with a brownish color to white center as they age. If left untreated, rapid defoliation occurs. Both blights have a similar overall effect. The leaves turn yellow, brown, wither, and die."
+        return "causes numerous small brown spots (about 1/8' to 1/4' in diameter) with a brownish color to white center as they age. If left untreated, rapid defoliation occurs. Both blights have a similar overall effect. The leaves turn yellow, brown, wither, and die."
 
     elif "Bacterial Spot" in predicted:
-        return "Bacterial spot of tomato is a highly dangerous disease that can result in unmarketable fruit and even death of the plant in severe cases. Bacterial spot can occur anywhere tomatoes are grown, but it is most common in warm, wet climates and in greenhouses."
+        return "a highly dangerous disease that can result in unmarketable fruit and even death of the plant in severe cases. Bacterial spot can occur anywhere tomatoes are grown, but it is most common in warm, wet climates and in greenhouses."
 
     else:
         return "No Data Found."
@@ -132,15 +137,19 @@ async def predict(
 
     # Definitions
     definition = getDefinition(predicted_class)
+    tagalog_definition = translate(definition)
 
     # Solution
     solution = getSolution(predicted_class)
+    tagalog_solution = translate(solution)
 
     return {
         "class": predicted_class,
         "confidence": float(confidence),
         "definition": definition,
-        "solution": solution
+        "solution": solution,
+        "tagalog_definition": tagalog_definition,
+        "tagalog_solution": tagalog_solution
     }
 
 if __name__ == "__main__":
